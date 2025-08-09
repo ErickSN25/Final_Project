3a7d57090b43ae2bbaadaaf3fb77b8213538ba45
yrsmzx <lorranaft123@gmail.com>
2025-08-09 17:17:30 -0300

    Algumas alterações

--- SerraVet/settings.py
+++ SerraVet/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Administrador',
-    'Atendente',
-    'Tutor',
-    'Veterinario',
+    'clinica',
 ]
 
 MIDDLEWARE = [
